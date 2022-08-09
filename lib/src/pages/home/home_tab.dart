74794d90840f09c0c65c014cf96531fd7dda667f
Naiara <naiara@mentorspace.dev>
2022-08-08 21:18:08 -0300

    Removendo importação desnecessária

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -2,7 +2,6 @@ import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/common/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common/custom_shimmer.dart';
