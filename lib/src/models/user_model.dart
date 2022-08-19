91b22ecabb7d851c0f2f88c09ab779771a109eac
Naiara <naiara@mentorspace.dev>
2022-08-18 21:53:55 -0300

    Adicionando o JsonSerializable

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,4 +1,10 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class UserModel {
+  @JsonKey(name: 'fullname')
   String? name;
   String? email;
   String? phone;
@@ -17,32 +23,14 @@ class UserModel {
     this.token,
   });
 
-  factory UserModel.fromMap(Map<String, dynamic> map) {
-    return UserModel(
-      cpf: map['cpf'],
-      email: map['email'],
-      id: map['id'],
-      name: map['fullname'],
-      password: map['password'],
-      phone: map['phone'],
-      token: map['token'],
-    );
-  }
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 
-  Map<String, dynamic> toMap() {
-    return {
-      'cpf': cpf,
-      'email': email,
-      'id': id,
-      'fullname': name,
-      'password': password,
-      'phone': phone,
-      'token': token,
-    };
-  }
+  /// Connect the generated [_$UserModelToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
 
   @override
   String toString() {
-    return 'name: $name | cpf: $cpf';
+    return 'UserModel(name: $name, email: $email, phone: $phone, cpf: $cpf, password: $password, id: $id, token: $token)';
   }
 }
