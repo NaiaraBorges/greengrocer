d1d7d9aa6597a17baf62ac5922115c20a79d2a58
Naiara <naiara@mentorspace.dev>
2022-08-18 21:09:28 -0300

    Criando repositório auth, fromJson e toJson do usuário

---
+++ lib/src/constants/endpoints.dart
@@ -0,0 +1,6 @@
+const String baseUrl = 'https://parseapi.back4app.com/functions';
+
+abstract class Endpoints {
+  static const String signin = '$baseUrl/login';
+  static const String signup = '$baseUrl/signup';
+}
