e96106de3e38d3c29e485020fd8b0ad1c56289cc
Naiara <naiara@mentorspace.dev>
2022-08-19 14:29:18 -0300

    Salvando token localmente

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/constants/storage_keys.dart';
 import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/pages/auth/repository/auth_repository.dart';
 import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
@@ -14,8 +15,43 @@ class AuthController extends GetxController {
   UserModel user = UserModel();
 
   Future<void> validateToken() async {
-    //TODO Recuperar o token que foi salvo localmente
-    //authRepository.validateToken(token);
+    // Recuperar o token que foi salvo localmente
+    String? token = await utilsServices.getLocalData(key: StorageKeys.token);
+
+    if (token == null) {
+      Get.offAllNamed(PagesRoutes.signInRoute);
+      return;
+    }
+
+    AuthResult result = await authRepository.validateToken(token);
+    result.when(
+      success: (user) {
+        this.user = user;
+        saveTokenAndProceedToBase();
+      },
+      error: (message) {
+        signOut();
+      },
+    );
+  }
+
+  Future<void> signOut() async {
+    //Zerar o user
+    user = UserModel();
+
+    //Remover o token localmente
+    await utilsServices.removeLocalData(key: StorageKeys.token);
+
+    //Ir para login
+    Get.offAllNamed(PagesRoutes.signInRoute);
+  }
+
+  void saveTokenAndProceedToBase() {
+    //Salvar o token
+    utilsServices.saveLocalData(key: StorageKeys.token, data: user.token!);
+
+    //Ir para a base
+    Get.offAllNamed(PagesRoutes.baseRoute);
   }
 
   Future<void> signIn({
@@ -32,7 +68,7 @@ class AuthController extends GetxController {
     result.when(
       success: (user) {
         this.user = user;
-        Get.offAllNamed(PagesRoutes.baseRoute);
+        saveTokenAndProceedToBase();
       },
       error: (message) {
         utilsServices.showToast(
--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 class UtilsServices {
+  final storage = const FlutterSecureStorage();
+
+  // Salva dado localmente em segurança
+  Future<void> saveLocalData({
+    required String key,
+    required String data,
+  }) async {
+    await storage.write(key: key, value: data);
+  }
+
+  // Recupera dado salvo localmente em segurança
+  Future<String?> getLocalData({required String key}) async {
+    return await storage.read(key: key);
+  }
+
+  //Remove dado salvo localmente
+  Future<void> removeLocalData({required String key}) async {
+    await storage.delete(key: key);
+  }
+
   String pricetoCurrency(double price) {
     NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR');
 
